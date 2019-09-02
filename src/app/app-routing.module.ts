a3c985b123b37113aa26b20c7430217461d1bb3e
Gerardo Pérez <gerardonoeperez@outlook.es>
2019-09-01 19:55:19 -0500

    Se añadió ruta para login y el componente login
    Se añadieron los componentes para el login así como se añadió la configuración de la ruta para ese componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
+  {path: '', component: AppComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
